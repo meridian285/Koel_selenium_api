130dd791f2e2735ae9fb74d452ce37619edd6f2f
Aleksandr Elizarov <akelizarovav@gmail.com>
2023-02-16 10:16:55 +0300

    added clearData method in the BaseTest.java

--- src/test/java/oi/koel/ui/config/BaseTest.java
+++ src/test/java/oi/koel/ui/config/BaseTest.java
@@ -3,9 +3,11 @@ package oi.koel.ui.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import oi.koel.ui.core.BasePage;
 import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.html5.WebStorage;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -17,18 +19,25 @@ public abstract class BaseTest {
     protected static WebDriver driver;
     protected static WebDriverWait wait;
 
-    @Before
-    public void setUp(){
+    @BeforeClass
+    public static void setUp(){
         WebDriverManager.chromedriver().setup();
+//        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         BasePage.setDriver(driver);
     }
+
     @After
-    public void tearDown(){
-        driver.close();
+    public void clearData(){
+        driver.manage().deleteAllCookies();
+        ((WebStorage) driver).getLocalStorage().clear();
+        ((WebStorage) driver).getSessionStorage().clear();
+    }
+    @AfterClass
+    public static void tearDown(){
         driver.quit();
     }
 }
